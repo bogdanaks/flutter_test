c6405ffbcd2392d7421705f0309fcdb995c4af11
bogdanaksenov <bogdanaks@bk.ru>
2021-03-14 21:32:44 +0300

    Add

--- lib/pages/main/weapons/widgets/TabScreen.dart
+++ lib/pages/main/weapons/widgets/TabScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:my_app/data/weapons.dart';
+import 'package:my_app/pages/main/weapons/widgets/WeaponImage.dart';
 
 class TabScreen extends StatelessWidget {
   final List weapons;
@@ -12,12 +13,11 @@ class TabScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return (
       ListView.separated(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
         itemCount: weapons.length,
         separatorBuilder: (BuildContext context, int index) => Divider(),
         itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text(WeaponsData['$type'][index]['title'], style: TextStyle(color: Colors.white, fontSize: 23),),
-          );
+          return WeaponImage(WeaponsData['$type'][index]['image']);
         },
       )
     );
